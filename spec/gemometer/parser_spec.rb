b2d4b7b42ffe0b3a056d5b31ab84bf1624ae5f54
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2015-11-16 22:22:31 -0200

    simplify specs

--- spec/gemometer/parser_spec.rb
+++ spec/gemometer/parser_spec.rb
@@ -12,10 +12,7 @@ describe Gemometer::Parser do
         expect(subject.parse).to eql([
           { name: 'aws-sdk',    newest: '2.1.32', installed: '1.66.0', requested: '= 1.66.0', group: 'default' },
           { name: 'byebug',     newest: '6.0.2',  installed: '5.0.0',  requested: '~> 5.0.0', group: 'development' },
-          { name: 'rollbar',    newest: '2.5.1',  installed: '2.4.0',  requested: '= 2.4.0',  group: 'default' },
           { name: 'shoulda',    newest: '3.0.1',  installed: '2.8.0',  requested: '~> 2.8',   group: 'test' },
-          { name: 'sidekiq',    newest: '3.5.1',  installed: '3.5.0',  requested: '= 3.5.0',  group: 'default' },
-          { name: 'hashie',     newest: '3.4.3',  installed: '3.4.2',  requested: nil,        group: nil },
           { name: 'minitest',   newest: '5.8.2',  installed: '5.8.1',  requested: nil,        group: nil },
           { name: 'multi_json', newest: '1.11.2', installed: '1.9.3',  requested: nil,        group: nil }
         ])
--- spec/support/mock_system.rb
+++ spec/support/mock_system.rb
@@ -7,10 +7,7 @@ Resolving dependencies...
 Outdated gems included in the bundle:
   * aws-sdk (newest 2.1.32, installed 1.66.0, requested = 1.66.0) in group "default"
   * byebug (newest 6.0.2, installed 5.0.0, requested ~> 5.0.0) in group "development"
-  * rollbar (newest 2.5.1, installed 2.4.0, requested = 2.4.0) in group "default"
   * shoulda (newest 3.0.1, installed 2.8.0, requested ~> 2.8) in group "test"
-  * sidekiq (newest 3.5.1, installed 3.5.0, requested = 3.5.0) in group "default"
-  * hashie (newest 3.4.3, installed 3.4.2)
   * minitest (newest 5.8.2, installed 5.8.1)
   * multi_json (newest 1.11.2, installed 1.9.3)
 )
